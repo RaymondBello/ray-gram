209cfe262cf51df49b24983a67046958ea34897e
ray-ab <raymondbello11@gmail.com>
2020-08-02 17:58:56 -0400

    Updates

---
+++ precache-manifest.0c7fc5978c9a19bd8ac2d5df4ba3b4ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c476a01d25dd7424a5a10e7194453652",
+    "url": "/ray-gram/index.html"
+  },
+  {
+    "revision": "8adc6ab7cef62f9e1e24",
+    "url": "/ray-gram/static/css/main.e8ce6da0.chunk.css"
+  },
+  {
+    "revision": "5f5bed6a4e27cff08431",
+    "url": "/ray-gram/static/js/2.930e37de.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/ray-gram/static/js/2.930e37de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8adc6ab7cef62f9e1e24",
+    "url": "/ray-gram/static/js/main.f1965ec1.chunk.js"
+  },
+  {
+    "revision": "0266b06cbe053711965e",
+    "url": "/ray-gram/static/js/runtime-main.4dc74347.js"
+  }
+]);
